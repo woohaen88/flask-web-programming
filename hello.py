ec62f21129b1a88a5a697e801115e529379c773a
woohaen88 <woohaen88@gmail.com>
2021-11-11 10:51:52 +0000

    라우팅 path parameter

--- hello.py
+++ hello.py
@@ -2,7 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/index')
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/users/<username>')
+def get_user(username):
+    return username
+
+@app.route('/posts/<int:post_id>')
+def get_post(post_id):
+    return str(post_id)
+
+@app.route('/uuid/<uuid:uuid>')
+def get_uuid(uuid):
+    return str(uuid)
